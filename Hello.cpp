0ed4860bf485e2191a63c13961bf722287891bae
scottgulbin <sc.gulbin@gmail.com>
2019-11-21 13:19:57 -0500

    Add project files.

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+//Hello.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Type your name" << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello, " << name << std::endl;
+
+	return 0;
+}
